857c35375ed677eaf775b621bd8bf0350687f658
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-07-14 10:10:55 -0600

    feat: Solutions to the grocery_grocery_list_array and suitcase_hash warm ups

--- lessons/hashes/warm_up/grocery_list_array.rb
+++ lessons/hashes/warm_up/grocery_list_array.rb
@@ -7,5 +7,18 @@ fridge_items_1 = ["milk", "eggs", "eggs","eggs", "eggs",
 
 # 1. What is problematic about listing `fridge_items_1`
 # as an array structured grocery list?
+# => You have to manually count how many of x tiems there are
+# => You will need to create additional code to iterate over the array to determine how many of each to receive
+# => Ordering matter in an array
+# => Things can get unorganized with arrays
 
 # 2. What might be a more readable way to structure this?
+# => Using some sort of key/value pair (key => item, value => quantity)
+
+p fridge_items_1.tally
+
+# fridge_items_1 = [
+#   []
+#   []
+#   []
+# ]
--- lessons/hashes/warm_up/suitcase_hash.rb
+++ lessons/hashes/warm_up/suitcase_hash.rb
@@ -3,6 +3,24 @@ suitcase = {
   "pants" => 1,
 }
 
+require 'pry';binding.pry
+
 # 1. Add a key/value pair of shirts/3 to the suitcase.
+suitcase["shirts"] => 3
+
 # 2. Increase the value of shirts by 1.
+suitcase["shirts"] => 4
+
+suitcase["shirts"] = suitcase["shirts"] + 1 # These two lines are exactly the same
+suitcase["shirts"] += 1                    # The code above is the breakdown of what the += 1 means
+
 # 3. Add a key/value pair of swimsuit/true to the suitcase.
+suitcase["swimsuits"] => true
+
+# To delete the swimsuit.
+suitcase.delete("swimsuits")
+
+# To get "0" for all
+suitcase = Hash.new(0)        # Note that this must be the first thing you do in your block. 
+
+suitcase.default = 0   # Another way to do it and would be done after the block is built
