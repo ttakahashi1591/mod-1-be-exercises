157e5cc4b7f5452bbd0a7edc2f1aa13428c72b2d
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-07-24 11:28:16 -0600

    feat: Add nested iteration work

--- lessons/nested_iteration/lesson/exercise/cities_lived_in.rb
+++ lessons/nested_iteration/lesson/exercise/cities_lived_in.rb
@@ -9,20 +9,36 @@ cities_lived_in = {
 # Problem #1: 
 # Get a unique list of all of the cities that these humans have lived in  
 # ["Philadelphia", "Fort Collins", "Seattle", "Denver", "Santa Fe", "Portland", "Lansing", "Columbus", "Austin"]
+all_cities = []
 
+cities_lived_in.each do |person, cities|
+  cities.each do |city|
+    unless all_cities.include?(city)
+      all_cities.push(city)
+    
+    end
+  end
+end
 
+p all_cities
+# cities_array = cities_lived_in.values.flatten.uniq
 
+# p cities_array
 
 # Problem #2: 
 # Write code that iterates through the `cities_lived_in` hash, and returns a list of  
 # names of the humans who've lived in Philadelphia.
 
-# [:michaela, :mike, :salvador]
+# [:michaela, :mike, :alex]
 # or
-# ["Michaela", "Mike", "Salvador"]
+# ["Michaela", "Mike", "Alex"]
+names = []
 
+cities_lived_in. each do |key, value|
+  names << key if value.include?("Philadelphia")
+end
 
-
+p names
 
 # Problem #3: 
 # Create a hash that has the city as a key, and the number of people that live in it as it's value: 
@@ -38,3 +54,29 @@ cities_lived_in = {
 #     "Columbus => 1,
 #     "Austin" => 1
 # }
+
+# cities_population = {}
+
+# cities_lived_in.each do |person|
+#   person[1].each do |city|
+#     if !cities_population.keys.include?(city)
+#       cities_population[city] = 1
+#     else
+#       cities_population[city] += 1
+#     end
+#   end
+# end
+
+# p cities_population
+
+by_city = Hash.new(0)
+
+cities_lived_in.values.each do |city_array|
+  city_array.each do |city|
+    by_city[city] += 1
+  end
+end
+
+p by_city
+
+p cities_lived_in.values.flatten.tally
--- lessons/nested_iteration/lesson/warmup/warmup.rb
+++ lessons/nested_iteration/lesson/warmup/warmup.rb
@@ -1,13 +1,60 @@
+# Part One
+# Given the follow array:
+# animals = [:dog, :cat, :zebra, :quokka, :unicorn, :bear]
+
+# Use an enumerable to
+# 1. Return an array of animals as strings.
+# p animals.map { |animal| animal.to_s }
+
+# string_animals = []
+
+# animals.each do |animal|
+#  string_animals << animal.to_s
+# end
+
+# p string_animals
+
+# 2. Return an array of animals with a length >= 4.
+# p animals.find_all { |animal| animal.length >= 4 }
+
+
+# Part Two
 # Given the following array:
 nested_animals = [[:dog, :cat, :zebra], [:quokka, :unicorn, :bear]]
 
 # Use an enumerable to:
 # 1. Return an unnested (single layer) array of animals as strings
-
+p nested_animals.flatten.map { |animal| animal.to_s }
 
 
 # 2. Return an unnested array of animals with length >= 4
+p nested_animals.flatten.find_all { |animal| animal.length >= 4 }
 
+selected_animals = []
 
+nested_animals.each do |group|
+  group.each do |animal|
+    if animal.length >= 4
+      selected_animals << animal
+    end
+  end
+end
+
+p selected_animals
 
 # 3. Return a hash where the keys are the animal, and the values are the length. ex: {dog: 3, cat: 3, zebra: 5 ... }
+objects = nested_animals.flatten.each_with_object({}) do |animal, hash|
+  hash[animal] = animal.length
+end
+
+p objects
+
+animals_hash = {}
+
+nested_animals.each do |group|
+  group.each do |animal|
+    animals_hash[animal] = animal.length  
+  end
+end
+
+p animals_hash
